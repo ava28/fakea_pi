9cde4e0a0c764009a21e16753ea56906f4ef0bd9
Abraham Vega <abraham.vega@autozone.com>
2022-03-08 15:47:15 -0700

    first commit

---
+++ db.js
@@ -0,0 +1,8 @@
+const db = require(".db");
+const tags = require("./data/tags");
+const brands = require("./data/brands");
+const itemTypes = require("./data/itemTypes");
+
+module.exports = {
+    db
+}
