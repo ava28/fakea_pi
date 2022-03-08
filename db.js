807d275e47e6607d983e1a04541210319e034c00
Abraham Vega <abraham.vega@autozone.com>
2022-03-08 15:48:21 -0700

    finish

--- db.js
+++ db.js
@@ -1,7 +1,4 @@
 const db = require(".db");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
 
 module.exports = {
     db
