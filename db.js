89663c0dcd2460188673e43d6d4a07d987c518ae
Abraham Vega <abraham.vega@autozone.com>
2022-03-08 16:51:43 -0700

    finish

--- db.js
+++ db.js
@@ -1,5 +1,38 @@
-const db = require(".db");
+const Comites = require("./data/Comites");
+const DatosInternos = require("./data/DatosInternos");
+const Directorio = require("./data/Directorio");
+const Estados = require("./data/Estados");
+const Usuarios = require("./data/usuarios");
 
 module.exports = {
-    db
+  Comites,
+  DatosInternos,
+  Directorio,
+  Estados,
+  Usuarios
 }
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
